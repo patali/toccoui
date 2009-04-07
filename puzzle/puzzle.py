9d20b1fd8cffbe6174f9f7ff118c6e2018b67208
sharath.patali <sharath.patali@81f65cc6-01bf-11de-87c9-111d11f6bf44>
2009-04-07 05:52:30 +0000

    puzzle:abstracted the snappable class

--- puzzle/puzzle.py
+++ puzzle/puzzle.py
@@ -11,60 +11,23 @@ from pyglet.media import *
 from pyglet.gl import *
 import random
 
-class PyzzleEngine(MTWidget):
-    def __init__(self, max=16, **kargs):
-        MTWidget.__init__(self, **kargs)
-        self.max        = max
-        self.pieces  = {}
-        self.gridHolders = {}
-        self.player = Player()
-        self.player.volume = 0.0
-        self.source = pyglet.media.load('super-fly.avi')
-        self.sourceDuration = self.source.duration
-        self.player.queue(self.source)
-        self.player.eos_action = 'loop'
-        self.width = self.player.get_texture().width
-        self.height = self.player.get_texture().height
-        self.player.play()
-        #puzzle_texture = pyglet.image.load('simpson.jpg')
-        puzzle_seq = pyglet.image.ImageGrid(self.player.get_texture(),4,4)
-        
-        self.griddy = MTGridLayout(rows=4,cols=4,spacing=1)
-        for i in range(self.max):
-            self.gridHolders[i] = gridHolder(size=(puzzle_seq[i].width,puzzle_seq[i].height))
-            self.griddy.add_widget(self.gridHolders[i])
-              
-        self.griddy.x = int((self.griddy.width))
-        self.griddy.y = int((self.griddy.height))
-        self.add_widget(self.griddy) 
-        self.griddy.pos = (int(w.width/2-self.griddy._get_content_width()/2),int(w.height/2-self.griddy._get_content_height()/2))
-        self.griddy._get_content_width()
-        
-        for i in range(self.max):
-            self.pieces[i] = PyzzleObject(image=puzzle_seq[i],grid=self.griddy)
-            self.add_widget(self.pieces[i])
-        
-class gridHolder(MTRectangularWidget):
-    def __init__(self, **kwargs):
-        super(gridHolder, self).__init__(**kwargs)
 
-            
-class PyzzleObject(MTWidget):
+#A snappable grid layout which produces a container to hold the snappable objects            
+class MTSnappableGrid(MTGridLayout):
     def __init__(self, **kwargs):
-        super(PyzzleObject, self).__init__(**kwargs)
-        self.image = kwargs.get('image')
-        self.x = int(random.uniform(100, 1000))
-        self.y = int(random.uniform(100, 800))
-        self.width = self.image.width
-        self.height = self.image.height
+        super(MTSnappableGrid, self).__init__(**kwargs)
+        self.block_size = kwargs.get('block_size')
+        self.gridHolders = {}
+        for i in range(self.rows*self.cols):
+            self.gridHolders[i] = MTRectangularWidget(size=(self.block_size[0],self.block_size[1]),bgcolor=(0.5,0.5,0.5))
+            self.add_widget(self.gridHolders[i])    
+
+#A snappable object which snaps into position in the grid            
+class MTSnappableWidget(MTWidget):
+    def __init__(self, **kwargs):
+        super(MTSnappableWidget, self).__init__(**kwargs)       
         self.state = ('normal', None)
         self.grid = kwargs.get('grid')
-
-    def draw(self):
-        glPushMatrix()
-        glColor4f(1,1,1,1)
-        self.image.blit(self.x,self.y,0)
-        glPopMatrix()
         
     def on_touch_down(self, touches, touchID, x, y):
         if self.collide_point(x,y):
@@ -81,20 +44,59 @@ class PyzzleObject(MTWidget):
     def on_touch_up(self, touches, touchID, x, y):
         if self.state[1] == touchID:
             self.state = ('normal', None)
-            for i in range(4):
-                for j in range(4):
-                    if(((self.center[0]>=int(self.grid.x+self.image.width*i)) & \
-                    (self.center[0]<=int(self.grid.x+self.image.width+self.image.width*i))) &\
-                    ((self.center[1]>=int(self.grid.y+self.image.height*j)) & \
-                    (self.center[1]<=int(self.grid.y+self.image.height+self.image.height*j)))
+            for i in range(self.grid.rows):
+                for j in range(self.grid.cols):
+                    if(((self.center[0]>=int(self.grid.x+self.width*j)) & \
+                    (self.center[0]<=int(self.grid.x+self.width+self.width*j))) &\
+                    ((self.center[1]>=int(self.grid.y+self.height*i)) & \
+                    (self.center[1]<=int(self.grid.y+self.height+self.height*i)))
                     ):
-                        #print "inside ",int(self.grid.x+self.image.width*i),int(self.grid.y+self.image.height*j)
-                        self.center = int(self.grid.x+self.image.width*i+self.image.width/2),int(self.grid.y+self.image.height*j+self.image.height/2)
-            return True
+                        self.center = int(self.grid.x+self.width*j+self.width/2),int(self.grid.y+self.height*i+self.height/2)
+            return True       
+            
+class PyzzleEngine(MTWidget):
+    def __init__(self, max=16, **kwargs):
+        MTWidget.__init__(self, **kwargs)
+        self.pieces  = {}
+        self.player = Player()
+        self.player.volume = 0.0
+        self.source = pyglet.media.load('super-fly.avi')
+        self.sourceDuration = self.source.duration
+        self.player.queue(self.source)
+        self.player.eos_action = 'loop'
+        self.width = self.player.get_texture().width
+        self.height = self.player.get_texture().height
+        self.player.play()
+        puzzle_seq = pyglet.image.ImageGrid(self.player.get_texture(),4,3)
+        
+        self.griddy = MTSnappableGrid(rows=4,cols=3,spacing=0,block_size=(puzzle_seq[0].width,puzzle_seq[1].height))
+        self.add_widget(self.griddy) 
+        
+        self.griddy.pos = (int(w.width/2-self.griddy._get_content_width()/2),int(w.height/2-self.griddy._get_content_height()/2))
+        
+        for i in range(self.griddy.rows*self.griddy.cols):
+            self.pieces[i] = PyzzleObject(image=puzzle_seq[i],grid=self.griddy)
+            self.add_widget(self.pieces[i])
+
+class PyzzleObject(MTSnappableWidget):
+    def __init__(self, **kwargs):
+        super(PyzzleObject, self).__init__(**kwargs)
+        self.image = kwargs.get('image')
+        self.x = int(random.uniform(100, 1000))
+        self.y = int(random.uniform(100, 800))
+        self.width = self.image.width
+        self.height = self.image.height
+
+    def draw(self):
+        glPushMatrix()
+        glColor4f(1,1,1,1)
+        self.image.blit(self.x,self.y,0)
+        glPopMatrix()
+       
 
 if __name__ == '__main__':
     w = MTWindow()
-    pyzzle = PyzzleEngine(max=16)
+    pyzzle = PyzzleEngine(max=12)
     w.add_widget(pyzzle)
     runTouchApp()
  
