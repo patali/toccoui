5845057b683ec1ed01b1aaea3359a5f1c3338aa0
sharath.patali <sharath.patali@81f65cc6-01bf-11de-87c9-111d11f6bf44>
2009-05-10 14:10:01 +0000

    mediaviewer: new mediaviewer without musicplayer

--- mediaviewer/mediaviewer.py
+++ mediaviewer/mediaviewer.py
@@ -1,12 +1,8 @@
-IS_PYMT_PLUGIN = True
-PLUGIN_TITLE = 'Video Player'
-PLUGIN_AUTHOR = 'Archana Nayak'
-PLUGIN_DESCRIPTION = 'This shows the media player'
 
 from pymt import *
 from pyglet.media import *
 from pymt.ui.widgets.videowidget import *
-import os
+import random
 
 if __name__ == '__main__':
     w = MTWindow()
@@ -14,7 +10,7 @@ if __name__ == '__main__':
     video3 = MTVideo(video='super-fly.avi',pos=(450,400))
     kin.add_widget(video3)
     w.add_widget(kin)
- ''''       for i in range (5):
+    for i in range (5):
         img_src = 'images/IMG'+str(i)+'.jpg'
         x = int(random.uniform(100, w.width-100))
         y = int(random.uniform(100, w.height-100))
@@ -22,5 +18,13 @@ if __name__ == '__main__':
         kin = MTKinetic()
         b = MTScatterImage(filename=img_src, pos=(x,y), size=(320,240), rotation=rot)
         kin.add_widget(b)
-        w.add_widget(kin)''''
-    runTouchApp()
+        w.add_widget(kin)
+        
+    exitbut = MTImageButton(filename="exit.png")
+    exitbut.x = int(w.width-exitbut.width)
+    exitbut.y = int(w.height-exitbut.height)    
+    w.add_widget(exitbut)
+    @exitbut.event    
+    def on_press(touchID, x, y):
+        sys.exit()
+    runTouchApp()        
