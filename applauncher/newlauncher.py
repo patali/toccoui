b0da0042905c1162ca045369819778dcb96e96f1
tommy.bjorling <tommy.bjorling@81f65cc6-01bf-11de-87c9-111d11f6bf44>
2009-07-29 20:11:45 +0000

    Project application: newlauncher.py is now ported. Also fixed the scatter effect. Still a bug with launching apps, but thats a core bug.

--- applauncher/newlauncher.py
+++ applauncher/newlauncher.py
@@ -20,15 +20,13 @@ def action_close_all(menu, w, args):
     
 def action_launch_plugin(menu, w, args):
     name, plugin = args
-    win = MTInnerWindow(size=(1024,768), pos=w.pos)
+    win = MTInnerWindow(size=(640,480), pos=w.pos)
     plugins.activate(plugin, win)
     menu.parent.add_widget(win)
-    win.fullscreen()
-    #action_close_menu(menu, w, None)
+    #win.fullscreen() # This will be fixed in core, and work later on.
+    action_close_menu(menu, w, None)
 
-#
-#This is the class definattion for text which appears over the icon
-#
+# This is the class definattion for text which appears over the icon
 class IconText(MTWidget):
     def __init__(self, **kwargs):
         kwargs.setdefault('size', (200,100))
@@ -42,10 +40,9 @@ class IconText(MTWidget):
         drawLabel(self.label,self.pos,False,(255,255,255,self.opacity))
 
 
-#This is a function which draws the label using pyglet and opengl
-
+# This is a function which draws the label using pyglet and opengl
 def drawLabel(text, pos=(0,0),center=True,textcolor=(255,255,255,75)):
-    _standard_label = Label(text='standard Label', font_size=200,bold=True, color=textcolor)
+    _standard_label = Label(text='standard Label', font_size=200, bold=True, color=textcolor)
     _standard_label.anchor_x = 'center'
     _standard_label.anchor_y = 'center'
     _standard_label.x = 0
@@ -58,10 +55,9 @@ def drawLabel(text, pos=(0,0),center=True,textcolor=(255,255,255,75)):
     glPopMatrix()
 
 
-#This is a class defination for the Icon object, the object of these classes maitain
-#its own properties like size, sense whether its at the center of the screen, change
-#the label's opacity, even detected doubletap events    
-
+# This is a class defination for the Icon object, the object of these classes maintain
+# its own properties like size, sense whether its at the center of the screen, change
+# the label's opacity, even detected doubletap events    
 class MTicon(MTButton):
     def __init__(self, **kwargs):
         kwargs.setdefault('scale', 1.0)
@@ -71,13 +67,13 @@ class MTicon(MTButton):
 
         super(MTicon, self).__init__(**kwargs)
         self.fname = kwargs.get('filename')
-        img                 = pyglet.image.load(kwargs.get('filename'))
-        self.image          = pyglet.sprite.Sprite(img)
-        self.image.x        = self.x
-        self.image.y        = self.y
-        self.scale          = kwargs.get('scale')
-        self.image.scale    = self.scale
-        self.width,self.height  = (self.image.width, self.image.height)
+        img = pyglet.image.load(kwargs.get('filename'))
+        self.image = pyglet.sprite.Sprite(img)
+        self.image.x = self.x
+        self.image.y = self.y
+        self.scale = kwargs.get('scale')
+        self.image.scale = self.scale
+        self.width,self.height = (self.image.width, self.image.height)
         self.texture = img.get_texture()
         self.label = kwargs.get('label')
         self.iconlabel = IconText(pos=(w.width/2,w.height/2+200))
@@ -88,29 +84,48 @@ class MTicon(MTButton):
         self.args = kwargs.get('args')
                 
     def draw(self):
-        self.image.x        = self.x
-        self.image.y        = self.y       
-        self.size           = (self.image.width, self.image.height)
+        self.image.x = self.x
+        self.image.y = self.y       
+        self.size = (self.image.width, self.image.height)
         with DO(gx_blending, gx_enable(GL_TEXTURE_2D)):
             set_color(1, 1, 1, 1)
             drawCover(self.texture.id, pos=(self.x,self.y), size=(self.image.width,self.image.height))
         self.parent.do_layout()
 
-    def on_touch_down(self, touches, touchID, x, y):        
-        if self.collide_point(x,y):
+    def on_touch_down(self, touch):        
+        if self.collide_point(touch.x, touch.y):
             self.iconlabel.show()
             self.start_animations('fadein')
-            if touches[touchID].is_double_tap:
+            if touch.is_double_tap:
                 self.iconlabel.hide()
-                self.action(self.parent, self, self.args)                
+                self.action(self.parent, self, self.args)
             return
 
-    def on_touch_move(self, touches, touchID, x, y):
+    def on_touch_move(self, touch):
         return
 
-    def on_touch_up(self, touches, touchID, x, y):
+    def on_touch_up(self, touch):
         self.iconlabel.hide()
         return        
+
+    def on_draw(self):
+        if (self.parent.parent.to_parent(self.x,self.y)[0] >= (w.width/2-128)) & (self.parent.parent.to_parent(self.x,self.y)[0] <= (w.width/2+128)):
+            self.y -= 50
+            if self.image.scale < 1.0:
+                self.image.scale = self.image.scale+math.sin(math.pi / 2 * 0.06)
+            self.width,self.height = (self.image.width, self.image.height)
+            self.iconlabel.label = self.label
+            self.iconlabel.show()
+            if self.iconlabel.opacity < 100:
+               self.iconlabel.opacity = self.iconlabel.opacity+5
+        else:
+            if self.image.scale > 0.5:
+                self.image.scale = self.image.scale-math.sin(math.pi / 2 * 0.06)
+                self.iconlabel.opacity = 0
+            self.width,self.height = (self.image.width, self.image.height)
+            self.iconlabel.hide()
+        self.draw()
+
 
     """def on_draw(self):
         if (self.parent.parent.to_parent(self.x,self.y)[0] >= (w.width/2-128)) & (self.parent.parent.to_parent(self.x,self.y)[0] <= (w.width/2+128)):
@@ -130,33 +145,39 @@ class MTicon(MTButton):
             #self.iconlabel.hide()
         self.draw()"""
 
-#This is a function which draws the Icons along with reflections
-
+# This is a function which draws the Icons along with reflections
 def drawCover(texture, pos=(0,0), size=(1.0,1.0)):
     with gx_enable(GL_TEXTURE_2D):
         glBindTexture(GL_TEXTURE_2D,texture)
-        #Draw First Cover
+        
+        # Draw First Cover
         pos = ( pos[0],pos[1],   pos[0]+size[0],pos[1],   pos[0]+size[0],pos[1]+size[1],  pos[0],pos[1]+size[1] )
         texcoords = (0.0,0.0, 1.0,0.0, 1.0,1.0, 0.0,1.0)
         draw(4, GL_QUADS, ('v2f', pos), ('t2f', texcoords))
-        #Draw S#econd Cover
+        
+        # Draw Second Cover
         pos2 = ( pos[0],pos[1]-size[1],   pos[0]+size[0],pos[1]-size[1],   pos[0]+size[0],pos[1]+size[1]-size[1],  pos[0],pos[1]+size[1]-size[1] )
         texcoords2 = (0.0,1.0, 1.0,1.0, 1.0,0.0, 0.0,0.0)
         color2 = (0,0,0,0, 0,0,0,0, 0.4,0.4,0.4,0, 0.4,0.4,0.4,0 )
         draw(4, GL_QUADS, ('v2f', pos2), ('t2f', texcoords2), ('c4f', color2))
 
 
-		
 if __name__ == '__main__':
     w = MTWindow(style={'bg-color':(0,0,0,1)})
-    layme = MTBoxLayout(padding=10, spacing=10, color=(0,0,0,1.0),pos=(75,300))
-    w.add_widget(layme)
     
+    plane = MTScatterPlane(bgcolor=(1,1,1,1.0),do_rotation=False, do_scale=False, do_translation=['x'], size=(1440,300),pos=(-128,w.height/2-150))
+    kiney = MTKinetic()
+    kiney.add_widget(plane)
+    w.add_widget(plane)
+    
+    layme = MTBoxLayout(padding=10, spacing=10, color=(0,0,0,1.0))
+    plane.add_widget(layme)
+
     plist = plugins.list()
     while len(plist):
         name, plugin = plist.popitem()
         infos = plugins.get_infos(plugin)
-        icon = MTicon(filename = infos.get('icon'),scale=0.6,label=infos.get('title'),action=action_launch_plugin, args=[name, plugin])
+        icon = MTicon(filename = infos.get('icon'),scale=0.5,label=infos.get('title'),action=action_launch_plugin, args=[name, plugin])
         layme.add_widget(icon)
-   
+    
     runTouchApp()
